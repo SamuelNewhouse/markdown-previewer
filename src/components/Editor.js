c9b87dfc7f50227041cf3548194e9a47674b0065
unknown <samuel_newhouse@yahoo.com>
2019-04-18 10:41:37 -0500

    Set up <Editor> as a controlled component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { updateInput } from '../actions/index';
+import { connect } from 'react-redux';
 
-class Editor extends Component {
-  render() {
-    return <textarea />
+const Editor = ({ value, onChange }) => {
+  return <textarea value={value} onChange={onChange} />
+}
+
+const mapStateToProps = state => {
+  return {
+    value: state.input
   }
 }
 
-export default Editor;
+const mapDispatchToProps = dispatch => {
+  return {
+    onChange: event => dispatch(updateInput(event.target.value))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor);
