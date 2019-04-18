e151d5a5cf9caa353ce401108c4e68801f8e2efb
unknown <samuel_newhouse@yahoo.com>
2019-04-18 10:43:44 -0500

    Setup marked.js options for parsing input text

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,22 @@
-import { UPDATE_INPUT } from '../actions'
+import { UPDATE_INPUT } from '../actions';
+import marked from 'marked';
+
+const renderer = new marked.Renderer();
+renderer.link = (href, title, text) => {
+  if (!title)
+    title = "";
+
+  return `
+  <a href="${href}" title="${title}" target="_blank">${text}</a>
+  `
+}
+
+marked.setOptions({
+  renderer: renderer,
+  gfm: true,
+  breaks: true,
+  sanitize: true
+});
 
 const initialState = { input: "" };
 
