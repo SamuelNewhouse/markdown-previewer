1f6ac13ab418d55032691ae1e9e712bff159aff9
unknown <samuel_newhouse@yahoo.com>
2019-04-22 15:05:14 -0500

    Add shrink and enlarge actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
 export const UPDATE_INPUT = 'UPDATE_INPUT'
+export const SHRINK_WINDOW = 'SHRINK_WINDOW'
+export const ENLARGE_WINDOW = 'ENLARGE_WINDOW'
 
 export function updateInput(input) {
   return { type: UPDATE_INPUT, input }
+}
+
+export function shrinkWindow(window) {
+  return { type: UPDATE_INPUT, window }
+}
+
+export function enlargeWindow(window) {
+  return { type: UPDATE_INPUT, window }
 }
