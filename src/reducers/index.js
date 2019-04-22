1d9e5d7ab1597244fdb146186fa579a9bf8dd47e
unknown <samuel_newhouse@yahoo.com>
2019-04-22 15:06:06 -0500

    Update reducer to handle shrink and enlarge

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,35 @@
-import { UPDATE_INPUT } from '../actions';
 import parseMarkdown from '../util/parseMarkdown';
+import { UPDATE_INPUT, SHRINK_WINDOW, ENLARGE_WINDOW } from '../actions';
+import {
+  NORMAL,
+  createWindowStatesFromEnlarge,
+  createWindowStatesFromShrink
+} from '../util/windowStates';
 
-const initialState = { input: "", output: "" };
+const initialState = {
+  input: "",
+  output: "",
+  editor: NORMAL,
+  preview: NORMAL
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+
     case UPDATE_INPUT:
       return Object.assign({}, state, {
         input: action.input,
         output: parseMarkdown(action.input)
       });
+
+    case SHRINK_WINDOW:
+      return Object.assign({}, state,
+        createWindowStatesFromShrink(state.editor, state.preview, action.window));
+
+    case ENLARGE_WINDOW:
+      return Object.assign({}, state,
+        createWindowStatesFromEnlarge(state.editor, state.preview, action.window));
+
     default:
       return state;
   }
