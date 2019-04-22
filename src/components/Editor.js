d3e27648162cc7b071322b4edd683ea65a5adb02
unknown <samuel_newhouse@yahoo.com>
2019-04-22 15:07:09 -0500

    Add basic Editor WindowBar

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { updateInput } from '../actions/index';
 import { connect } from 'react-redux';
+import WindowBar from './common/WindowBar'
+import editorIcon from '../svg/pencil.svg';
+import copyIcon from '../svg/copy.svg';
+import shrinkIcon from '../svg/shrink2.svg';
+import enlargeIcon from '../svg/enlarge2.svg';
+
+const bar = {
+  info: {
+    icon: editorIcon,
+    title: 'Editor',
+    name: 'editor'
+  },
+  buttons: [
+    {title: 'Copy', icon: copyIcon, onClick: 'PLACEHOLDEr'},
+    {title: 'Shrink', icon: shrinkIcon, onClick: 'PLACEHOLDEr'},
+    {title: 'Enlarge', icon: enlargeIcon, onClick: 'PLACEHOLDEr'}
+  ]
+
+}
 
 const Editor = ({ value, onChange }) => {
-  return <textarea
-    className="m-3 bg-dark text-white border border-primary p-2"
-    id="editor"
-    value={value}
-    onChange={onChange}
-  />
+  return (
+    <div className="m-3 content">
+      <WindowBar bar={bar} />
+      <textarea
+        className="bg-dark text-white border border-primary p-2"
+        id="editor"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
