0da06fc418117eea718d07deedd83d50da3eba10
unknown <samuel_newhouse@yahoo.com>
2019-04-29 10:34:25 -0500

    Create notifyCopy action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const UPDATE_INPUT = 'UPDATE_INPUT'
 export const SHRINK_WINDOW = 'SHRINK_WINDOW'
 export const ENLARGE_WINDOW = 'ENLARGE_WINDOW'
+export const NOTIFY_COPY = 'NOTIFY_COPY'
 
 export function updateInput(input) {
   return { type: UPDATE_INPUT, input }
@@ -12,4 +13,8 @@ export function shrinkWindow(window) {
 
 export function enlargeWindow(window) {
   return { type: ENLARGE_WINDOW, window }
+}
+
+export function notifyCopy(message) {
+  return { type: NOTIFY_COPY, message }
 }
