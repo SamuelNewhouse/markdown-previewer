9943d58b87ffa0a151946f52d2a74d5d43131aa1
unknown <samuel_newhouse@yahoo.com>
2019-04-29 12:15:51 -0500

    Updates

---
+++ precache-manifest.600bdf11ab36e5db129ea227edada3df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e1548a19386586171194cb71fe5539",
+    "url": "/SamuelNewhouse/markdown-previewer/index.html"
+  },
+  {
+    "revision": "dbd611ced0d7c3f640a0",
+    "url": "/SamuelNewhouse/markdown-previewer/static/css/2.647cd933.chunk.css"
+  },
+  {
+    "revision": "1129af20c2f0c62ec9c0",
+    "url": "/SamuelNewhouse/markdown-previewer/static/css/main.cb9a9504.chunk.css"
+  },
+  {
+    "revision": "dbd611ced0d7c3f640a0",
+    "url": "/SamuelNewhouse/markdown-previewer/static/js/2.9d3fa9ef.chunk.js"
+  },
+  {
+    "revision": "1129af20c2f0c62ec9c0",
+    "url": "/SamuelNewhouse/markdown-previewer/static/js/main.7e4f7734.chunk.js"
+  },
+  {
+    "revision": "7976e4f6e39e76955f34",
+    "url": "/SamuelNewhouse/markdown-previewer/static/js/runtime~main.7175dfe9.js"
+  },
+  {
+    "revision": "d663d5d774a75f37c487a35f823f3af5",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/enlarge2.d663d5d7.svg"
+  },
+  {
+    "revision": "6955fcfe5cb557e6fc4e6f7c9bb566ca",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/eye.6955fcfe.svg"
+  },
+  {
+    "revision": "cb24418778b27826b13d31b7016326c5",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/htmlcopy.cb244187.svg"
+  },
+  {
+    "revision": "b2c3197e7f75053ba4d24422756d81f7",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/mdcopy.b2c3197e.svg"
+  },
+  {
+    "revision": "8809d3cf95ee20c74e5ebbdc6ee556da",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/pencil.8809d3cf.svg"
+  },
+  {
+    "revision": "7905d19ed62e19b886748964ec1cd66f",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/question.7905d19e.svg"
+  },
+  {
+    "revision": "2e08aa9f292bc54e2611102acc61c5db",
+    "url": "/SamuelNewhouse/markdown-previewer/static/media/shrink2.2e08aa9f.svg"
+  }
+]);
